9d019fbe57b2d8dafbf77d0b02027daf6125ffa0
jinified <jinified@gmail.com>
2018-12-27 00:49:06 +0800

    Added adminCreateUser API

--- src/config/app.js
+++ src/config/app.js
@@ -1,3 +1,6 @@
-module.exports = {
-    DOMAIN: 'astro-tap-portal.s3-website-ap-southeast-1.amazonaws.com'
-}
+export default {
+    DOMAIN: 'astro-tap-portal.s3-website-ap-southeast-1.amazonaws.com',
+    API_ENDPOINT: {
+        'management': 'https://jmf40o6ho9.execute-api.ap-southeast-1.amazonaws.com/dev'
+    }
+};
